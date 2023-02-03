eb5488503440063eaee71a8b86b9fadc1302c116
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2023-02-03 15:10:02 +0000

    Make the scheduler scalable
    Scheduler is now able to run on many different nodes

--- index.js
+++ index.js
@@ -1,65 +1,78 @@
-const http = require("http");
-const { exec } = require("child_process");
-const { promisify } = require("util");
-const execAsync = promisify(exec);
-const requestListener = async function (req, res) {
-  console.log("I've got a message!");
-  if (req.method == "POST") {
-    let body = "";
-    req.on("data", (data) => {
-      body += data;
-    });
-    req.on("end", () => {
-      const data = JSON.parse(body);
-      spawnProcess(data.language);
-      res.writeHead(200);
-      res.end();
-    });
-  } else {
-    res.writeHead(200);
-    res.end();
-  }
-};
-const server = http.createServer(requestListener);
-server.listen(3300);
+const DockerEvents = require("docker-events");
+const Dockerode = require("dockerode");
+const docker = new Dockerode();
+const emitter = new DockerEvents({ docker });
 
-function spawnProcess(language) {
-  let image = "";
+emitter.on("connect", function() {
+  console.log("connected to docker api");
+});
+
+emitter.on("die", function(message) {
+  spawnProcess(message.from);
+});
+
+emitter.start();
+
+function getImageForLanguage(language) {
   switch (language) {
     case "JavaScript":
-        image = "ghcr.io/mbruty/codelab-codeengine/bun-engine";
-      break;
+      return "ghcr.io/mbruty/mike-codelab-codeengine/bun-engine";
     case "TypeScript":
-        image = "ghcr.io/mbruty/codelab-codeengine/typescript-engine";
-      break;
+      return "ghcr.io/mbruty/mike-codelab-codeengine/typescript-engine";
     case "C#":
-        image = "ghcr.io/mbruty/codelab-codeengine/dotnet-engine";
-      break;
+      return "ghcr.io/mbruty/mike-codelab-codeengine/dotnet-engine";
+    case "Python":
+      return "ghcr.io/mbruty/mike-codelab-codeengine/python-engine";
     default:
       throw "No engine found";
   }
+}
+
+function spawnProcess(image) {
   // Run the docker image
   // -d detached
   // --rm remove the container after is is finished
   // ${image} The docker image to use
   // -m Limit the memory usage to 512mb
   // -cpus="1" Limit the amount of cpus to 1
-  exec(`docker run -d --rm ${image} -m 512m -cpus="1" `);
+    ["-d", "--rm", "-m 512", "-cpus=1"],
+    // 
+  docker.run(
+    image,
+    "",
+    null,
+    {
+      HostConfig: {
+        Memory: 5.12e8, // Max 512MB of RAM
+        DiskQuota: 5.12e8, // Max 512 MB, prevent zip bombs
+        AutoRemove: true,
+      }
+    }
+  );
 }
 
 // Always keep atleast 1 container running at all times
 // This will speed up response times considerably
 
-async function startRunning(image) {
-  const x = await execAsync(`docker ps | grep ${image} | wc -l`);
-  if (parseInt(x.stdout) != 0) {
-    console.log(image, " is already running");
-  } else {
-    console.log("warming up ", image);
-    exec(`docker run -d --rm ${image} -m 512m -cpus="1" `);
+async function warmup(numRequired) {
+  const images = {
+    // "ghcr.io/mbruty/mike-codelab-codeengine/bun-engine": 0,
+    "ghcr.io/mbruty/mike-codelab-codeengine/typescript-engine": 0,
+    // "ghcr.io/mbruty/mike-codelab-codeengine/dotnet-engine": 0,
+    "ghcr.io/mbruty/mike-codelab-codeengine/python-engine": 0
   }
+  const containers = await docker.listContainers();
+  for (const container of containers) {
+    images[container.Image]++;
+  }
+
+  for (const image in images) {
+    const numToCreate = numRequired - images[image];
+    for (let i = 0; i < numToCreate; i++) {
+      spawnProcess(image);
+    }
+  }
+
 }
 
-startRunning("ghcr.io/mbruty/codelab-codeengine/typescript-engine");
-startRunning("ghcr.io/mbruty/codelab-codeengine/dotnet-engine");
-startRunning("ghcr.io/mbruty/codelab-codeengine/bun-engine");
+warmup(2);
